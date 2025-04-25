be9a7d5223e7ac10ed5ce809ff95e2ec9a8347b3
Karz <karan_shukla@live.com>
2025-04-25 19:52:49 -0400

    create a testing form

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,17 +1,55 @@
 import {
   Box,
-  Center,
   Heading,
   Text,
   VStack,
-  HStack,
   Icon,
-  Link,
   Container,
+  Input,
+  HStack,
+  Button,
+  Stack,
+  Field,
 } from '@chakra-ui/react';
 import { FaHammer, FaGithub, FaDiscord } from 'react-icons/fa';
+import { useForm } from 'react-hook-form';
+
+interface FormValues {
+  username: string
+  email: string
+  password: string
+}
+
+function onSubmit(data: FormValues) {
+  console.log(data);
+  fetch('http://localhost:3000/api/register', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error('Registration failed');
+      }
+      return response.json();
+    })
+    .then((responseData) => {
+      console.log('Registration successful:', responseData);
+    })
+    .catch((error) => {
+      console.error('Error during registration:', error);
+    });
+}
 
 export function App() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormValues>()
+
   return (
     <Box
       display="flex"
@@ -29,6 +67,30 @@ export function App() {
             <Text fontSize="xl" >
               Currently under construction!
             </Text>
+            <form onSubmit={handleSubmit(onSubmit)}>
+      <Stack gap="4" align="flex-start" maxW="sm">
+
+        <Field.Root invalid={!!errors.username}>
+          <Field.Label>Username</Field.Label>
+          <Input {...register("username")} />
+          <Field.ErrorText>{errors.username?.message}</Field.ErrorText>
+        </Field.Root>
+
+        <Field.Root invalid={!!errors.email}>
+          <Field.Label>Email Address</Field.Label>
+          <Input {...register("email")} />
+          <Field.ErrorText>{errors.email?.message}</Field.ErrorText>
+        </Field.Root>
+
+        <Field.Root invalid={!!errors.password}>
+          <Field.Label>Password</Field.Label>
+          <Input disabled={true} {...register("password")} />
+          <Field.ErrorText>{errors.password?.message}</Field.ErrorText>
+        </Field.Root>
+
+        <Button type="submit">Submit</Button>
+      </Stack>
+    </form>
           </VStack>
       </Container>
     </Box>
