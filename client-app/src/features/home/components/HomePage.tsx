b965f9b6c4acaccebdb61fe1dbf71b03a6e4d479
Karz <karan_shukla@live.com>
2025-05-31 08:30:49 -0400

    styling fixes

--- client-app/src/features/home/components/HomePage.tsx
+++ client-app/src/features/home/components/HomePage.tsx
@@ -16,43 +16,35 @@ const HomePage: React.FC = () => {
     <Container maxW="container.xl">
       <VStack gap={6} py={10} align="flex-start">
         <Prose>
-          <Heading as="h1" size="2xl" mb={4}>
-            TWT Tournament App
-          </Heading>
           <Text>
             Create custom brackets, participate in Total War Warhammer 3
             tournaments within the multiplayer community, and view statistics
             for recorded matchups.
           </Text>
         </Prose>
-        <Text>
-          Want to view an ongoing tournament? Enter in the Tourney Code below:
-        </Text>
-        <Input placeholder="Enter Tourney Code" />
-        <Button colorScheme="teal" size="lg">
-          View Tournament
-        </Button>
+        <HStack>
+          <Text>
+            Want to view an ongoing tournament? Enter in the Tourney Code here:
+          </Text>
+          <Input placeholder="Enter Tourney Code" />
+          <Button colorScheme="teal" size="lg">
+            View Tournament
+          </Button>
+        </HStack>
         <Text>
           Want to participate in an upcoming tournament? Head over to the
           "Tournaments" page using the navigation bar
         </Text>
       </VStack>
-      <HStack
-        justify="center"
-        align="center"
-        gap={8}
-        py={10}
-        flexDirection={{ base: "column", md: "row" }}
-      >
-        <Prose>
-          Want to participate in a tournament? Create a guest account and change
-          your username
-        </Prose>
-        <Prose>
-          Want to host tournaments? Create an account and login to access
-          tournament management features
-        </Prose>
-      </HStack>
+      <Prose>
+        Want to participate in a tournament? Create a guest account and change
+        your username
+      </Prose>
+      <br></br>
+      <Prose>
+        Want to host tournaments? Create an account and login to access
+        tournament management features
+      </Prose>
     </Container>
   );
 };
