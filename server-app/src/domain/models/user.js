1a039d3a304dd191c4d69a21ae9f22f42c3b3f7c
Karz <karan_shukla@live.com>
2026-05-10 19:33:17 -0400

    add railway

--- server-app/src/domain/models/user.js
+++ server-app/src/domain/models/user.js
@@ -2,9 +2,9 @@ import bcrypt from "bcrypt";
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true, index: true },
+  username: { type: String, required: true },
   password: { type: String, required: false },
-  email: { type: String, required: true, index: true },
+  email: { type: String, required: true },
   createdAt: { type: Date, default: Date.now },
 });
 
